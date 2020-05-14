80e5b7b92e8e780312977313ccc5f18c0a1d8176
Joseph Monaco <jmonaco@jhu.edu>
2020-05-14 09:42:35 -0400

    Handle dense and sparse delay projections

--- floyd/base/projection.py
+++ floyd/base/projection.py
@@ -6,14 +6,13 @@ __all__ = ('BaseProjection',)
 
 
 import copy
-from numbers import Number
+import numbers
 
 from toolbox.numpy import *
 from specify import Specified, Param
 from roto.dicts import pprint as dict_pprint
 
 from ..matrix import pairwise_distances as distances
-from ..delay import DelayLines
 from ..state import State
 
 from .groups import BaseUnitGroup
@@ -42,14 +41,12 @@ class BaseProjection(Specified, BaseUnitGroup):
     p = Param(True, doc='connectivity specification')
     kernel_fanout = Param(0.1, doc='fanout specification if p is a KernelFunc')
     allow_multapses = Param(False, doc='a connection may comprise >1 contacts')
-    signal_dtype = Param('f', doc='output signal dtype')
+    signal_dtype = Param('d', doc='output signal dtype')
     
     # Projection variables:
     #
     # C -- binary {0, 1} connectivity (adjacency) matrix
     # S -- integer connectivity matrix of total contacts per connection
-    # delays -- transmission delay matrix (in simulation timesteps)
-    # cursor -- cursor matrix indicating delay loop phase of each connection
 
     base_variables = ('C', 'S')
     base_dtypes = {'C': 'u1', 'S': 'u2'}
@@ -63,27 +60,29 @@ class BaseProjection(Specified, BaseUnitGroup):
         self.pre = pre
         self.post = post
         self.recurrent = pre is post
-        self.terminal = zeros(self.shape, self.signal_dtype)  # signal readout
+
+        # Public terminal for accessing projection output
+        self.terminal = zeros(self.shape, self.signal_dtype)
 
         if State.is_defined('network'):
             State.network.add_projection(self)
             
-        self._initialized = True
+        self._last_t = -1
         self._connected = False
-        self._usedelays = False
+        self._initialized = True
 
     def print_stats(self):
         """
         Print the parameters and connectivity statistics for these synapses.
         """
         if not self._connected:
-            raise RuntimeError('this projection is currently not connected')
+            raise RuntimeError('projection is currently not connected')
 
         msd = lambda x: '{:.3g} +/- {:.3g}'.format(np.nanmean(x), np.nanstd(x))
         nz = self.fanin.nonzero()
         stats = dict(
             connectivity_fraction = '{:.3f}'.format(
-                float(self.N_conns / (self.pre.size * self.post.size))),
+                float(self.N_connections / (self.pre.size * self.post.size))),
             fanout_units = f'{msd(self.fanout)}',
             fanin_units = f'{msd(self.fanin)}',
             fanout_contacts = f'{msd(self.S.sum(axis=0))}',
@@ -93,37 +92,64 @@ class BaseProjection(Specified, BaseUnitGroup):
         )
         self.out.printf(dict_pprint(stats, name=f'Projection({self.name})'))
 
-    def set_delays(self, delays, timebased=False, dense=True, delay_dtype='u4'):
+    def set_delays(self, delays, *, timebased=False, dense=False, dtype='u4'):
         """
-        Add dense or sparse signal delay lines to the connections.
+        Add transmission delays to the connections in this projection.
         """
-        if dense == False:
-            # TODO Sparse delays like previous code
-            raise NotImplementedError('sparse delays')
-        
+        if not self._connected:
+            raise RuntimeError('run connect() before setting delays')
+
         if timebased:
             delays = np.round(delays / State.dt)
-
-        self.delays = 1 + expand_dims(delays, -1).astype(delay_dtype)
-        self.cursor = zeros(self.delays.shape, 'i4')
-        self.delay_lines = zeros(self.shape + (self.delays.max() + 1,), 
+        delays = expand_dims(delays, -1).astype(dtype)
+        
+        # Create array of delay timesteps
+        self._dense_delays = dense
+        if dense:
+            dshape = self.shape + (1,)
+            self._pre_idx = slice(None)
+            self._delay_axis = 2
+        else:
+            dshape = (self.N_connections, 1)
+            self._pre_idx = self.j
+            self._delay_axis = 1
+        self._delays = zeros(dshape, dtype)
+        self._delays[:] = delays
+        
+        # Create the delay cursor and lines
+        self._max_delay = self._delays.max()
+        self._zero_delays = self._max_delay == 0
+        self._cursor = self._delays.astype('i4') - 1
+        self._cursor[self._cursor < 0] = 0  # init zero-delays to index 0
+        self._lines = zeros(dshape[:-1] + (self._max_delay + 1,), 
                                     self.signal_dtype)
-
-        self._usedelays = True
 
     def update(self):
         """
-        Update the projection with current transmitter (pre) output signals.
+        Update the projection delay lines with transmitter (pre) outputs.
         """
-        if self._usedelays:
-            np.put_along_axis(self.delay_lines, self.cursor, 
-                              self.pre.output[AX], axis=2)
-            self.cursor[:] = (self.cursor + 1) % self.delays
-            self.terminal[:] = np.take_along_axis(self.delay_lines, 
-                                                  self.cursor, axis=2)
-            return
+        if State.t <= self._last_t: return
+
+        if self._zero_delays:
+            sigout = self.pre.output[self._pre_idx]
+        else:
+            # Update the transmitter outputs at the current cursor
+            np.put_along_axis(self._lines, self._cursor, 
+                self.pre.output[self._pre_idx,AX], axis=self._delay_axis)
+
+            # Increment the delay line cursor
+            self._cursor[:] = (self._cursor + 1) % (self._delays + 1)
+
+            # Set output to the new cursor position
+            sigout = np.take_along_axis(self._lines, self._cursor, 
+                axis=self._delay_axis)[...,0]
         
-        self.terminal[:] = self.pre.output[AX]
+        if self._dense_delays:
+            self.terminal[:] = sigout
+        else:
+            self.terminal[self.ij] = sigout
+
+        self._last_t = State.t
         
     def connect(self, **specs):
         """
@@ -134,7 +160,7 @@ class BaseProjection(Specified, BaseUnitGroup):
 
         # Update projection specs if provided in keyword arguments
         oldp = copy.deepcopy(self.p)
-        self.update(specs)
+        super().update(specs)
 
         # Set connection (C) and contact (S) matrices to zero in order to
         # build new projections based on the contact matrix
@@ -155,7 +181,7 @@ class BaseProjection(Specified, BaseUnitGroup):
                     self.S = eye(*self.shape)
                 elif 1 < self.p <= self.post.size:
                     if self.allow_multapses:
-                        for j in range(self.pre.size)):
+                        for j in range(self.pre.size):
                             conn = self.rnd.randint(self.post.size, size=self.p)
                             for i in conn:
                                 self.S[i,j] += 1  # increment contact counts
@@ -175,7 +201,7 @@ class BaseProjection(Specified, BaseUnitGroup):
 
          # Eliminate autapses (cycles) if this is a recurrent projection
         if self.recurrent:
-            self.S *= 1 - eye(self.pre.size)
+            self.S *= 1 - eye(self.pre.size).astype(self.S.dtype)
 
         # Compute the connectivity stats and maps
         self.compute_connectivity()
@@ -184,6 +210,7 @@ class BaseProjection(Specified, BaseUnitGroup):
         """
         Process the connectivity matrices into stats and useful data structures.
         """
+        self.inactive = (self.S == 0).nonzero()
         self.ij = self.S.nonzero()
         self.i, self.j = self.ij  # unzipped matrix indices of all connections
         self.C[self.ij] = 1  # binary {0,1} connectivity (adjacency) matrix
